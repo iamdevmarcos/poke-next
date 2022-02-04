12bd95a1e22e0f6db424e3a57055e4c02a3374aa
Marcos Andre <marcosdev.me@gmail.com>
2022-02-04 15:03:24 -0300

    feat: Add About Page and Styles

---
+++ pages/about.jsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+
+import styles from "../styles/About.module.css";
+
+export default function About() {
+  return (
+    <div className={styles.about}>
+      <h1>About this Project</h1>
+      <p>
+        Project created with Next.JS made for studies. <br />
+        Feel free to download this project or send feedback
+      </p>
+      <Image
+        src="/images/charizard.png"
+        width="300"
+        height="300"
+        alt="Charizard"
+        className={styles.image}
+      />
+    </div>
+  );
+}
