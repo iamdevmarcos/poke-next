d576d810a613d234a0c4cd5657c21d91b2259e13
Marcos Andre <marcosdev.me@gmail.com>
2022-02-04 16:09:28 -0300

    feat: Add API Fetch

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,33 @@
 import styles from "../styles/Home.module.css";
 
-export default function Home() {
-  return <h1>PokeNext</h1>;
+export async function getStaticProps() {
+  const maxPokemons = 252;
+  const api = "https://pokeapi.co/api/v2/pokemon/";
+
+  const res = await fetch(`${api}?limit=${maxPokemons}`);
+  const data = await res.json();
+
+  data.results.forEach((item, index) => {
+    item.id = index + 1;
+  });
+
+  return {
+    props: {
+      list: data.results,
+    },
+  };
+}
+
+export default function Home({ list }) {
+  return (
+    <div>
+      <h1>PokeNext</h1>
+
+      <ul>
+        {list.map((item) => (
+          <li key={item.id}>{item.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
