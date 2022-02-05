5d0155d61c8772e6d70a4eec4b9a73fe3948a79f
Marcos Andre <marcosdev.me@gmail.com>
2022-02-05 09:51:28 -0300

    perf: add ssr

--- pages/pokemon/[pokemonId].jsx
+++ pages/pokemon/[pokemonId].jsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 
 import styles from "../../styles/Pokemon.module.css";
 
+import { useRouter } from "next/router";
+
 export const getStaticPaths = async () => {
   const maxPokemons = 251;
   const api = "https://pokeapi.co/api/v2/pokemon/";
@@ -17,7 +19,7 @@ export const getStaticPaths = async () => {
 
   return {
     paths,
-    fallback: false,
+    fallback: true,
   };
 };
 
@@ -33,6 +35,12 @@ export const getStaticProps = async (context) => {
 };
 
 export default function Pokemon({ pokemon }) {
+  const router = useRouter();
+
+  if (router.isFallback) {
+    return <div>Loading...</div>;
+  }
+
   const avatar = `https://cdn.traction.one/pokedex/pokemon/${pokemon.id}.png`;
 
   return (
